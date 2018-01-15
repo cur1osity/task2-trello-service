ccbd6393f2821551e4ef1c932b96b98ef3fafcd9
cur1osity <marcin.gajc@gmail.com>
2018-01-15 17:13:59 +0100

    Eureka + docker init

--- src/main/java/com/cur1osity/trelloservice/TrelloserviceApplication.java
+++ src/main/java/com/cur1osity/trelloservice/TrelloserviceApplication.java
@@ -2,7 +2,9 @@ package com.cur1osity.trelloservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class TrelloserviceApplication {
 
